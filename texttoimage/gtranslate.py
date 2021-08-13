1f0f0b9fbdb52d35b2f8420b83b25d9261d042bf
asibkamalsada <asib1999@gmail.com>
2021-08-13 18:28:22 +0200

    puh

---
+++ texttoimage/gtranslate.py
@@ -0,0 +1,22 @@
+import requests
+
+api_url = r"https://translation.googleapis.com/language/translate/v2?key=AIzaSyAa5TXeL8kKbAaViyby-IrNocK3Jwltp_o"
+
+data = {
+    "q": "default",
+    "source": "en",
+    "target": "de",
+    "format": "text"
+}
+
+
+def translate(text):
+    data["q"] = text
+    response = requests.post(api_url, json=data)
+    print(response)
+    return response.json()["data"]["translations"][0]["translatedText"]
+
+
+with open("REVhpsentences.txt", "r") as inf:
+    with open("gtranslated.txt", "w+") as outf:
+        outf.writelines(translate(t) for t in inf.readlines())

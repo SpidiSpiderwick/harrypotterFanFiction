41c1817e7e3e8d0c52611a03c473a5593c67a6d8
asibkamalsada <asib1999@gmail.com>
2021-08-17 23:06:18 +0200

    task2 quali went live

--- mturkdevelopment/mturk.py
+++ mturkdevelopment/mturk.py
@@ -1,25 +1,30 @@
 import boto3
-import urllib.parse as url
 
 from rootkey import keyid, secretkey
 
-if __name__ == '__main__':
-    client = boto3.client('mturk', endpoint_url='https://mturk-requester-sandbox.us-east-1.amazonaws.com',
-                          region_name='us-east-1', aws_access_key_id=keyid, aws_secret_access_key=secretkey)
+sandbox = "https://mturk-requester-sandbox.us-east-1.amazonaws.com"
+production = "https://mturk-requester.us-east-1.amazonaws.com"
+client = boto3.client('mturk', endpoint_url=production, region_name='us-east-1', aws_access_key_id=keyid,
+                      aws_secret_access_key=secretkey)
+
+
+def create_quali():
     desc = "Diese Qualification zeigt, dass Sie in der Lage sind, einen grammatikalisch und orthographisch korrekten " \
            "deutschen Satz zu bilden. Außerdem ermöglicht sie Ihnen, unsere Tasks zur Satzsynthese durchzuführen."
-    with open("qualification.xml", "r") as quxml:
+    with open("qualification.xml", "r", encoding="utf-8") as quxml:
         test = quxml.read()
-        # test = url.quote(quxml.read(), safe="")
-    with open("answerkey.xml", "r") as anxml:
+    with open("answerkey.xml", "r", encoding="utf-8") as anxml:
         answerKey = anxml.read()
-        # answerKey = url.quote(anxml.read(), safe="")
-        print(answerKey)
     response = client.create_qualification_type(Name="GrammatikExpertIn",
                                                 Keywords="Grammatik,Satz,Proofread,Satz,Satz bilde,Satzsynthese",
                                                 Description=desc,
                                                 QualificationTypeStatus="Active",
                                                 Test=test,
                                                 AnswerKey=answerKey,
-                                                TestDurationInSeconds=1800,
+                                                TestDurationInSeconds=300,
+                                                # RetryDelayInSeconds=0,
                                                 AutoGranted=False)
+
+
+if __name__ == '__main__':
+    create_quali()
